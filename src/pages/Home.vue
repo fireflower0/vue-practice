f7927b073c4bbbaac7231953e0887339e5c130fe
fireflower0 <fireflower0@hotmail.co.jp>
2021-04-05 19:51:24 +0900

    First commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='home'>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+#home {
+  margin: 5px;
+  padding: 10px;
+  background-color: #FFF3E0;
+}
+</style>
